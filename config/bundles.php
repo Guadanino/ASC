8c3bde5c233fe3776b340a191035695aefdc9403
Romain GUIOT <romain.guiot@hotmail.fr>
2024-02-18 17:46:36 +0100

    CKEditor init

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
 ];
