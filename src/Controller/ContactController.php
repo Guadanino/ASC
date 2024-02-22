6397b6e1c824be8e3470f29918b8d794adb10648
Romain GUIOT <romain.guiot@hotmail.fr>
2024-02-22 15:40:55 +0100

    init

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,15 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
-    #[Route('/', name: 'app_contact_index', methods: ['GET'])]
-    public function index(ContactRepository $contactRepository): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'contacts' => $contactRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_contact_new', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app_contact_index', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $contact = new Contact();
@@ -40,42 +32,5 @@ class ContactController extends AbstractController
             'contact' => $contact,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_contact_show', methods: ['GET'])]
-    public function show(Contact $contact): Response
-    {
-        return $this->render('contact/show.html.twig', [
-            'contact' => $contact,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_contact_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Contact $contact, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_contact_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('contact/edit.html.twig', [
-            'contact' => $contact,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_contact_delete', methods: ['POST'])]
-    public function delete(Request $request, Contact $contact, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $contact->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($contact);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_contact_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,60 +22,11 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home/new', name: 'app_home_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $home = new Home();
-        $form = $this->createForm(HomeType::class, $home);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($home);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('home/new.html.twig', [
-            'home' => $home,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/home/{id}', name: 'app_home_show', methods: ['GET'])]
     public function show(Home $home): Response
     {
         return $this->render('home/show.html.twig', [
             'home' => $home,
         ]);
-    }
-
-    #[Route('/home/{id}/edit', name: 'app_home_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Home $home, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(HomeType::class, $home);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('home/edit.html.twig', [
-            'home' => $home,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/home/{id}', name: 'app_home_delete', methods: ['POST'])]
-    public function delete(Request $request, Home $home, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $home->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($home);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
     }
 }
