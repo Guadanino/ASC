a5915e1c9448b803dcfc988f9f6b199c893531a7
Romain GUIOT <romain.guiot@hotmail.fr>
2024-02-22 15:39:12 +0100

    administration

--- src/Controller/Admin/AdminHomeController.php
+++ src/Controller/Admin/AdminHomeController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/')]
-class HomeController extends AbstractController
+#[Route('/admin/home', name: 'admin_home_')]
+class AdminHomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(HomeRepository $homeRepository): Response
     {
         return $this->render('home/index.html.twig', [
@@ -22,7 +22,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home/new', name: 'app_home_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $home = new Home();
@@ -42,7 +42,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home/{id}', name: 'app_home_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Home $home): Response
     {
         return $this->render('home/show.html.twig', [
@@ -50,7 +50,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home/{id}/edit', name: 'app_home_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Home $home, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(HomeType::class, $home);
@@ -68,7 +68,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home/{id}', name: 'app_home_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_home_delete', methods: ['POST'])]
     public function delete(Request $request, Home $home, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete' . $home->getId(), $request->request->get('_token'))) {
